363068819aaf8008e88eb7e9a301fa37362d307a
Michael Flach <vuti@gmx.de>
2019-01-20 20:52:56 +0100

    delete script

---
+++ playground/mongo-db-delete.js
@@ -0,0 +1,40 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+	if(err) {
+		return console.log('Unable to connect to MongoDB server')
+	}
+	console.log('Connected to MongoDB server');
+	const db = client.db('TodoApp');
+
+	// deleteMany
+	// db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result);
+	// });
+
+	// deleteOne
+	// db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+	// 	console.log(result.result);
+	// });
+
+	// findeOneAndDelete
+	// db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+	// 	console.log(result);
+	// });
+
+
+	/***********************************************/
+
+	// db.collection('Users').deleteMany({name: 'Micha'}).then((result) => {
+	// 	console.log(result.result);
+	// });
+
+	db.collection('Users').findOneAndDelete({
+		_id: new ObjectID('5c44c2e19c4a92a6fa3d222e')
+	}).then((result) => {
+		console.log(JSON.stringify(result, undefined, 2));
+	});
+
+
+	client.close();
+});
